bfcf603f0b8c37db49209ba85b681db324dd76b0
FloBurke <burke.fwn@oundleschool.org.uk>
2023-11-29 11:43:22 +0000

    copy for poeple

---
+++ addpeople.php
@@ -0,0 +1,20 @@
+<?php
+try{
+    array_map("htmlspecialchars", $_POST);
+    include_once("connection.php");
+    
+    $stmt = $conn->prepare("INSERT INTO TblBooks (BookID,Author,Title,Genre)VALUES (null,:author,:title,:genre)");
+
+    $stmt->bindParam(':author', $_POST["author"]);
+    $stmt->bindParam(':title', $_POST["title"]);
+    $stmt->bindParam(':genre', $_POST["genre"]);
+    $stmt->execute();
+}
+catch(PDOException $e)
+	{
+		echo "error".$e->getMessage();
+	}
+$conn=null;
+header('Location: books.php');
+
+?>
