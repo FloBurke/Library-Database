fa9ab1dc9f8f5202f7de9c1b1465e5f7f160a125
FloBurke <burke.fwn@oundleschool.org.uk>
2023-12-06 10:11:46 +0000

    searching by genre

---
+++ search.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Books</title>
+</head>
+<body>
+<form action="searchbook.php" method="post">
+    
+    Genre:<select name="genre">
+            <option value="Horror">Horror</option>
+            <option value="Classic">Classic</option>
+            <option value="Non-Fiction">Non-fiction</option>
+            <option value="Kids">Kids</option>
+            <option value="Y-A">Yound-Adult</option>
+            <option value="Comedy">Comedy</option>
+        </select>
+        <input type="submit" value="search"><br>
+</form>
+<form action="searchbook.php" method="post">
+    
+    Genre:<select name="genre">
+            <option value="Horror">Horror</option>
+            <option value="Classic">Classic</option>
+            <option value="Non-Fiction">Non-fiction</option>
+            <option value="Kids">Kids</option>
+            <option value="Y-A">Yound-Adult</option>
+            <option value="Comedy">Comedy</option>
+        </select>
+        <input type="submit" value="search"><br>
+</form>
+</body>
+</html>
---
+++ searchbook.php
@@ -0,0 +1,12 @@
+<?php
+include_once("connection.php");
+    
+//print_r($_POST);
+$stmt = $conn->prepare("SELECT * FROM tblbooks WHERE Genre=:genre");
+$stmt->bindParam(':genre', $_POST["genre"]);
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+echo($row["Title"].' '.$row["Author"]."<br>");
+}
+?>
